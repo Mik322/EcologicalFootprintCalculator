d8a0ca68e73622f549c101c1b8c58f71e11238ee
Mik322 <mchsa@iscte-iul.pt>
2020-11-08 23:08:33 +0000

    Added save and load states functionalities

--- src/main/Application.scala
+++ src/main/Application.scala
@@ -1,12 +1,19 @@
 package main
 
+import scala.annotation.tailrec
+import fileOperations.FileOperations
+
+
 object Application extends App{
 
   val footPrintState = FootPrintState(0);
   val calorieCounter = CalorieCounter(0);
 
+  @tailrec
   def main_loop(footPrintState: FootPrintState, calorieCounter: CalorieCounter): Unit = {
-
+    main_loop(footPrintState, calorieCounter);
   }
+
+  main_loop(footPrintState, calorieCounter)
 
 }
--- src/main/fileOperations/FileOperations.scala
+++ src/main/fileOperations/FileOperations.scala
@@ -1,11 +1,29 @@
 package main.fileOperations
 
 import main.FootPrintState
+import main.CalorieCounter
+import java.io._
+
+
+case class States(footPrintState: FootPrintState, calorieCounter: CalorieCounter)
 
 object FileOperations {
-  def saveToFile(state: FootPrintState) = {}
+  def saveStates(footPrintState: FootPrintState, calorieCounter: CalorieCounter) = {
+    val states = States(footPrintState, calorieCounter)
+    try {
+      val out = new ObjectOutputStream(new FileOutputStream(new File("States")))
+      out.writeObject(states)
+    }
+  }
 
-  def readFromFile(): FootPrintState = {
-    FootPrintState(0)
+  def loadFootPrintState(): Option[(FootPrintState, CalorieCounter)] = {
+    try {
+      val in = new ObjectInputStream(new FileInputStream(new File("States")))
+      val states = in.readObject().asInstanceOf[States]
+      Some((states.footPrintState, states.calorieCounter))
+    } catch {
+      case _ => None
+    }
+
   }
 }
