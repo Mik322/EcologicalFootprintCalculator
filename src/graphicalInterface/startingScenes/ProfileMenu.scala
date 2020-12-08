9be9884d1a9326591b68f93d120fe8333fdc74c9
Mik322 <mchsa@iscte-iul.pt>
2020-12-08 22:38:40 +0000

    bug fix

--- src/graphicalInterface/startingScenes/ProfileMenu.scala
+++ src/graphicalInterface/startingScenes/ProfileMenu.scala
@@ -10,14 +10,14 @@ class ProfileMenu {
   var newProfileButton, loadProfileButton: Button = _
 
   def newProfile: Unit = {
-    val loader = new FXMLLoader(getClass.getResource("Questionary.fxml"))
+    val loader = new FXMLLoader(getClass.getResource("newprofile/NewProfile.fxml"))
     val root: Parent = loader.load()
 
     newProfileButton.getScene.setRoot(root)
   }
 
   def loadProfile: Unit = {
-    val fxmlLoader = new FXMLLoader(getClass.getResource("../HomePage.fxml"))
+    val fxmlLoader = new FXMLLoader(getClass.getResource("LoadProfile.fxml"))
     val root: Parent = fxmlLoader.load()
 
     loadProfileButton.getScene.setRoot(root)
