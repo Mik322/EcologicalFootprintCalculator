17da7c2607ab27142657942b03e5a1a46a053a90
Afonso Esteves <afonsomgesteves@gmail.com>
2020-12-11 12:41:45 +0000

    footprint png fixed

--- src/graphicalInterface/startingScenes/ProfileMenu.scala
+++ src/graphicalInterface/startingScenes/ProfileMenu.scala
@@ -3,11 +3,19 @@ package graphicalInterface.startingScenes
 import javafx.fxml.{FXML, FXMLLoader}
 import javafx.scene.Parent
 import javafx.scene.control.Button
+import javafx.scene.image.ImageView
+import javafx.scene.image.Image
 
 class ProfileMenu {
 
   @FXML
   var newProfileButton, loadProfileButton: Button = _
+  @FXML
+  var footprint: ImageView = _
+  @FXML
+  def initialize() = {
+    footprint.setImage(new Image("file:footprint.png"))
+  }
 
   def newProfile: Unit = {
     val loader = new FXMLLoader(getClass.getResource("newProfile/NewProfile.fxml"))
